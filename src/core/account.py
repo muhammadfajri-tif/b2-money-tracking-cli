7db3136fcfc90d778357398e16cb9407a9e586cd
muhammadfajri-tif <149763217+muhammadfajri-tif@users.noreply.github.com>
2024-03-23 20:40:50 +0700

    fix(transaction): resolve data types issue when increment total amount money

--- src/core/account.py
+++ src/core/account.py
@@ -47,7 +47,7 @@ class Account:
         self._transactions.append(transaction)
         # mutate field money
         transaction.amount = int(transaction.amount)
-        self._total_money += str(transaction.amount) if transaction.type == "income" else str(-transaction.amount)
+        self._total_money += int(transaction.amount) if transaction.type == "income" else int(-transaction.amount)
         #sort by date
         self._transactions = sorted(self._transactions, key=lambda trx: trx.date)
         # append to the file
